93b5cf3b5ec879390538fe558385d9951f896290
D Gopal Krishna <nikhil0223@gmail.com>
2021-01-08 18:49:20 +0530

    Able to get data in the front end from the database seamlessly.

--- Typing_practice_app/Symbiosis/views.py
+++ Typing_practice_app/Symbiosis/views.py
@@ -1,33 +1,80 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from json import dumps
-from PyPDF2 import PdfFileReader, PdfFileWriter
+from Symbiosis.models import Quotes
+import numpy as np
 
 # Create your views here.
 
 
 def rendertypingPage(request):
-    pdf_karma = open('Influence.pdf', 'rb')
-    pdf_file = PdfFileReader(pdf_karma, strict=False)
+    # n = 2
+    # rangy = np.arange(1, n+1)                   ## n= 2 as there's 2 data rows in databse
+    # choice_num = np.random.choice(rangy)
+
+    # list_of_quotes = []
+    # quotes_text = Quotes.objects.get(id=1)
+    # for i in range(1):
+    #     list_of_quotes.append(quotes_text[i])
+
+    # data = {'quotes_text': list_of_quotes}
+
+    # return render(request, 'Symbiosis/index.html', context=data)
+
+
     
-    page_1 = pdf_file.getPage(90)      
-    text_page_1 = page_1.extractText()
+
+    quotes_all = Quotes.objects.all()
     
-    after_split = text_page_1.split(" ")
-    n = 30
+    n = len(quotes_all)
+    rangy = np.arange(0, n)                   
+    choice_num = np.random.choice(rangy)
 
-    listy = []
-    for i in range(0,len(after_split),n):
-        listy.append(after_split[i:i+n])
+    list_of_quotes = []
+    for i in range(choice_num, choice_num+1):
+        list_of_quotes.append(quotes_all[i])
+
+    data = {'quotes_DB': list_of_quotes}                                              ## For rendering in HTML                                           ## For rendering in HTML
+
+    return render(request, 'Symbiosis/index.html', context=data)
+    
 
 
 
 
-    typeThis = "Its to test whether the .py string variable is accessible from .js file's string variable"
-    stringy = {
-        'typeThis': typeThis,
-        'listy': listy
-    }
-    stringy = dumps(stringy)
-    return render(request, 'Symbiosis/index.html', {'stringy' : stringy})
-    # return HttpResponse("Hi there")
+
+
+
+    # quotes_all = Quotes.objects.all()
+    # list_of_quotes = []
+    # for i in range(len(quotes_all)):
+    #     list_of_quotes.append(quotes_all[i])
+
+    # data = {'quotes_DB': list_of_quotes}                                              ## For rendering in HTML                                           ## For rendering in HTML
+
+    # return render(request, 'Symbiosis/index.html', context=data)
+    #
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ## For Normal String text
+    # typeThis = "Its to test whether the .py string variable is accessible from .js file's string variable"
+
+    # stringy = {
+    #     'typeThis': typeThis,
+    # }
+
+
+    # stringy = dumps(stringy)
+    # return render(request, 'Symbiosis/index.html', {'stringy' : stringy})
